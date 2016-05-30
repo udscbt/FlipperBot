dc0dc5270193e0f2ac9ea376547bad0fdae03ad8
fabio <test@example.com>
2016-05-30 02:38:22 +0200

    Implemented basic FBCP in Python

---
+++ FBCP/fbcp/__init__.py
@@ -0,0 +1,144 @@
+class Command:
+  code = 0
+  
+  Q = []
+  A = []
+  list = []
+  
+  def __init__ (self, id=None, params=None):
+    self.code = self.__class__.code
+    self.__class__.code = self.__class__.code + 1
+    self.id = id if id is not None else ""
+    self.params = params if params is not None else []
+  
+  def __eq__ (self, cmd):
+    if isinstance(cmd, self.__class__):
+      return self.code == cmd.code
+    elif isinstance(cmd, str):
+      return self.id == cmd
+    else:
+      return False
+  
+  def __str__ (self):
+    return self.id
+  
+  def __repr__ (self):
+    return "<%s.%s code=%d id='%s' params=%s>" % (self.__class__.__module__, self.__class__.__name__, self.code, self.id, repr(self.params))
+
+Command.NULL = Command()
+
+class Param:
+  code = 0
+  
+  STD = []
+  
+  def __init__ (self, id):
+    self.code = self.__class__.code
+    self.__class__.code = self.__class__.code + 1
+    self.id = id
+  
+  def __eq__ (self, par):
+    if isinstance(par, self.__class__):
+      return self.code == par.code
+    elif isinstance(par, str):
+      return self.id == par
+    else:
+      return False
+  
+  def __repr__ (self):
+    return "<%s.%s code=%d id='%s' >" % (self.__class__.__module__, self.__class__.__name__, self.code, self.id)
+
+
+class ProtocolException (Exception):
+  def __init__(self, msg="", extra=""):
+    self.msg = msg
+    self.extra = extra
+
+
+class CommandLine:  
+  def clear (self):
+    self.command = Command.NULL
+    self.params = {}
+    self.other = ""
+  
+  def __init__ (self, command=None, params=None, other=None):
+    self.clear()
+    if command is not None:
+      self.command = command
+    if params is not None:
+      self.params = params
+    if other is not None:
+      self.other = other
+    
+  def parse (self, msg):
+    self.clear()
+    
+    for cmd in Command.Q+Command.A:
+      if msg.startswith(cmd.id):
+        cmdlen = len(cmd.id)
+        if (len(msg) > cmdlen and msg[cmdlen] != " " and msg[cmdlen] != "\n"):
+          continue
+        self.command = cmd
+        s = msg[cmdlen:]
+        params = [x for x in s.split(" ") if x != ""]
+        npar = len(cmd.params)
+        if len(params) < npar:
+          raise ProtocolException("Missing parameter", "Expected %d parameters, got %d. In command '%s'" % (npar, len(params), cmd.id))
+        for i in range(npar):
+          self.params[cmd.params[i]] = params[i]
+        self.other = " ".join(params[npar:])
+        return True
+    self.command = Command.NULL
+    return False
+  
+  def write (self):
+    if self.command == Command.NULL:
+      raise ProtocolException("Nonexistent command")
+    s = self.command.id
+    for par in self.command.params:
+      if (not par in self.params.keys()):
+        raise ProtocolException("Missing parameter", "Unspecified value for parameter '%s' in command '%s'" % (par, self.command.id))
+      s += " %s" % self.params[par]
+    if self.other != "":
+      s += " %s" % self.other
+    return s
+  
+  def __str__ (self):
+    return self.write()
+  
+  def __repr__ (self):
+    return "<%s.%s command='%s' params=%s other='%s'>" % (self.__class__.__module__, self.__class__.__name__, self.command, repr(self.params), self.other)
+
+
+def loadCommands(filename="command_lists/fbcp.command.list"):
+  f = open(filename, "r")
+  content = f.read()
+  f.close()
+
+  for line in content.split("\n"):
+    line_s = line.strip()
+    if line_s.startswith("/"):
+      continue
+    if line_s == "":
+      continue
+    tokens = line.split("|")
+    cmd = tokens[0].strip().replace(" ", "_")
+    id  = tokens[1].strip().replace(" ", "_")
+    try:
+      params = tokens[2].strip().replace(" ", "_")
+      params = params.split(",")
+    except:
+      params = []
+    command = Command(id, params)
+    setattr(Command, cmd, command)
+    if cmd.startswith("Q_"):
+      Command.Q.append(command)
+      Command.list.append(command)
+    elif cmd.startswith("A_"):
+      Command.A.append(command)
+      Command.list.append(command)
+    else:
+      param = Param(id)
+      setattr(Param, cmd, param)
+      Param.STD.append(param)
+
--- FBCP/main.cpp
+++ FBCP/main.cpp
@@ -4,14 +4,21 @@
 
 void debugCommand(fbcp::COMMAND_LINE& cmd)
 {
-  printf("Command: %s\n", &(cmd.command->id[0]));
-  printf("Params:\n");
-  for (std::vector<fbcp::string>::const_iterator it = cmd.command->params.begin(); it != cmd.command->params.end(); ++it)
+  if (cmd.command == fbcp::NULL_COMMAND)
   {
-    std::map<fbcp::string, fbcp::string>::iterator found = cmd.params.find(*it);
-    printf("  [%s] = %s\n", &((*it)[0]), (found==cmd.params.end()?"<missing>":&(found->second[0])));
+    printf("NULL_COMMAND\n");
   }
-  printf("Other:\n  %s\n", &(cmd.other[0]));
+  else
+  {
+    printf("Command: %s\n", &(cmd.command->id[0]));
+    printf("Params:\n");
+    for (std::vector<fbcp::string>::const_iterator it = cmd.command->params.begin(); it != cmd.command->params.end(); ++it)
+    {
+      std::map<fbcp::string, fbcp::string>::iterator found = cmd.params.find(*it);
+      printf("  [%s] = %s\n", &((*it)[0]), (found==cmd.params.end()?"<missing>":&(found->second[0])));
+    }
+    printf("Other:\n  %s\n", &(cmd.other[0]));
+  }
   fflush(stdout);
 }
 
@@ -49,25 +56,6 @@ int main()
   
   debugCommand(cmd);
   debugCommand(cmd2);
-  
-  /*fbcp::string msg;
-  while (true)
-  {
-    fbcp::COMMAND_LINE cmd_q, cmd_a;
-    std::cout << "> ";
-    std::getline(std::cin, msg);
-    fbcp::parseCommand(msg+"\n", cmd_q);
-    if (cmd_q.command != fbcp::NULL_COMMAND)
-    {
-      debugCommand(cmd_q);
-    }
-    if (cmd_q.command == fbcp::NULL_COMMAND or !fbcp::common::handleRequest(cmd_q, cmd_a))
-    {
-      fbcp::common::handleNotFound(msg, cmd_a);
-    }
-    std::cout << fbcp::writeCommand(cmd_a);
-    debugCommand(cmd_a);
-  }*/
 
   return 0;
 }
