53f1f5b7baca032d022157dc3fbad3e313b4c2a2
fabio <test@example.com>
2016-06-02 17:38:37 +0200

    Added project Robot

---
+++ Projects/Robot/src/main.ino
@@ -0,0 +1,18 @@
+#include "schemo.h"
+#include "fbcp.common.h"
+#include "FBNet.h"
+#include <ESP8266WiFi.h>
+
+WiFiClient sockIn;
+WiFiClient sockOut;
+WiFiServer server (FBNet::PORT);
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
