3599a849d1df41ba215ed3152600e0c358b545a7
fabio <test@example.com>
2016-08-23 23:24:47 +0200

    ELABORATO DRAFT added to repository to avoid data loss

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from flipperbot.board import *
+g = game.Game(totems.totems.values(), debug=False, logging=False, displayex=False)
+s = server.Server(g, dbgFlag=True)
+s.address = '192.168.1.1'
+
+g.start()
+s.start()
