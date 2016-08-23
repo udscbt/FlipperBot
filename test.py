9c641e2a981f9ab97e4c652c1b49b38cbb3959b6
fabio <test@example.com>
2016-08-24 00:09:05 +0200

    Cleaning

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 from flipperbot.board import *
 g = game.Game(totems.totems.values(), debug=False, logging=False, displayex=False)
 s = server.Server(g, dbgFlag=True)
-s.address = '192.168.1.1'
+#s.address = '192.168.1.1'
+s.address = '0.0.0.0'
 
 g.start()
 s.start()
